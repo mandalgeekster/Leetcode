55067a2f2ba9bb6a9576ec638a44b4eddd5bb792
ABHISHEK PATIDAR <95904102+1e9abhi1e10@users.noreply.github.com>
2022-12-25 22:57:41 +0530

    Time: 32 ms (91.87%), Space: 13.8 MB (71.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,20 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
         
-        dp = [0 for _ in range(len(nums))]
-        dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
-        for index in range(2, len(nums)-1):
-            dp[index] = max((nums[index] + dp[index-2]), dp[index-1])
+        def house_robber(nums):
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2,len(nums)):
+                dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+            return max(dp[-1], dp[-2])
         
-        dp1 = [0 for _ in range(len(nums))]
-        lst = nums[::-1]
-        dp1[0] = lst[0]
-        dp1[1] = max(lst[0],lst[1])
-        for index in range(2,len(nums)-1):
-            dp1[index] = max((lst[index] + dp1[index-2]), dp1[index-1])
-        return max(max(dp),max(dp1))
+        if len(nums) <=2 : return max([0] + nums)
+        return max( house_robber(nums[1:]), house_robber(nums[:-1]) )
+  
         
         
