7747b0aa1a0980b40b26cd174d216e645192c973
ABHISHEK PATIDAR <2311abhiptdr@gmail.com>
2023-02-14 12:34:56 +0530

    Time: 245 ms (51.97%), Space: 23 MB (88.97%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -8,12 +8,21 @@ class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         stk = [root]
         ans = 0 
+      #  print(stk)
         while stk:
             node = stk.pop()
+          #  print(node)
             if low<=node.val<=high:
                 ans+=node.val
             if node.left:
                 stk.append(node.left)
+               # print(stk)
             if node.right:
                 stk.append(node.right)
+               # print(stk)
         return ans
+    
+    #[TreeNode{val: 10, left: TreeNode{val: 5, left: TreeNode{val: 3, left: None, right: None},
+# right: TreeNode{val: 7, left: None, right: None}},
+ # right: TreeNode{val: 15, left: None,
+ #  right: TreeNode{val: 18, left: None, right: None}}}]
