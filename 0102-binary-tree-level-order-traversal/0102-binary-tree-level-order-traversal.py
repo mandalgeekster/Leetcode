26db9d56e0e73db4f85dd6bfa3f3597e5fd4d3ee
Abhishek Patidar <2311abhiptdr@gmail.com>
2023-04-15 13:05:49 +0530

    Time: 39 ms (42.46%), Space: 14.3 MB (41.12%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: 
+        if root is None: 
             return []
         stack = [(root)]
         a = []
