e64ad084e678aa260933ea5f60cd9e1df09b06c3
Abhishek Patidar <2311abhiptdr@gmail.com>
2023-04-30 00:32:22 +0530

    Time: 150 ms (42.72%), Space: 57.2 MB (88.56%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        bool flag = true;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if ((nums[i] ^ nums[i+1]) == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return !flag;
+    }
+};
