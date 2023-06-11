01a18bc99f7cd3a6ac43df3e07d418cd23964437
Abhishek Patidar <2311abhiptdr@gmail.com>
2023-06-12 02:17:35 +0530

    Time: 63 ms (53.25%), Space: 17 MB (51.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,14 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        st = ""
-        cn = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        for i in s:
-            if i in cn:
-                st += i
-        low = st.lower()
-        return low == low[::-1]
-            
+    def isPalindrome(self, s):
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l <r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l +=1; r -= 1
+        return True
+                
         
