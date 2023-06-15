95afbca96b9b549d0d98d839d4cb6fb14b319945
Abhishek Patidar <2311abhiptdr@gmail.com>
2023-06-15 13:28:20 +0530

    Time: 49 ms (35.92%), Space: 16.4 MB (27.01%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,16 @@
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        
+        
+        def pows(x, n):
+            if abs(n) == 0:
+                return 1 
+            elif abs(n) % 2 ==0:
+                return pows(x * x, abs(n) // 2)
+            else:
+                return x * pows(x * x, (abs(n) - 1)//2)
+        
+            
+        return float(pows(x, n)) if n >= 0 else 1/pows(x, n)
         
